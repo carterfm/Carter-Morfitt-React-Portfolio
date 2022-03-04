8bb02d064808ec0ef9b38f476f477abe26fa3f5d
carterfm <carterf.morfitt@gmail.com>
2022-03-03 20:15:32 -0800

    Finished resume section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import resume from '../../resume/CarterMorfittResumeDraft.pdf';
+import '../../styles/Resume.css';
 
 function Resume () {
     return (
-        <h1>Resume stuff goes here</h1>
+        <div className="container-fluid text-center resume-page">
+            <p><a href={resume} download>Click here</a> to download my resume!</p>
+            <p>Front end skills:</p>
+            <ul className="frontend-skill-list">
+                <li>HTML5, CSS, JavaScript</li>
+                <li>JQuery, Bootstrap, React</li>
+            </ul>
+            <p className="font-weight-bold">Back end skills:</p>
+            <ul className="backend-skill-list">
+                <li>MySQL, Sequelize, MongoDB, Mongoose</li>
+                <li>Express, Node, Handlebars</li>
+            </ul>
+        </div>
     );
 }
+
+// ● Frontend: HTML5, CSS, JQuery, JavaScript, Bootstrap
+// ● Backend: MySQL, Sequelize, Express, Node, Handlebars
 
 export default Resume;
