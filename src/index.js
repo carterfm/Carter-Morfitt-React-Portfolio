055b2b32fcfebad30d366af425cb1c022e29b264
carterfm <carterf.morfitt@gmail.com>
2022-03-04 19:06:10 -0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-// Importing bootstrap so we have access to its classes 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
